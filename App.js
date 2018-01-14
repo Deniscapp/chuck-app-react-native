70e983a6efa8765babcd30affda4352689cc201e
deniscapp <denis_cappelini@hotmail.com>
2018-01-14 21:46:26 -0200

    declaring `StackNavigator` with its scenes

--- App.js
+++ App.js
@@ -6,30 +6,20 @@
 
 import React, { Component } from "react";
 import { Platform, StyleSheet, Text, View } from "react-native";
+import { StackNavigator } from 'react-navigation';
 
 import ChuckIntro from "./src/components/ChuckIntro";
+import ChuckList from "./src/components/ChuckList";
 
-export default class App extends Component<{}> {
-  render() {
-    return <ChuckIntro />;
-  }
-}
+const ChuckApp = StackNavigator(
+  {
+    Home: { screen: ChuckIntro },
+    FactsList: { screen: ChuckList }
+  },
+  {
+    initialRouteName: 'Home',
+  },
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
-  }
-});
+export default () => <ChuckApp />;
+
