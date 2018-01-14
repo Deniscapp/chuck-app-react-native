f4a796b14788eb52811bc265d38e789db7a84268
deniscapp <denis_cappelini@hotmail.com>
2018-01-14 21:49:24 -0200

    setting an `onPress`  function

--- src/components/Category.js
+++ src/components/Category.js
@@ -2,12 +2,11 @@ import React from "react";
 import { StyleSheet, View, Text } from "react-native";
 import { Badge } from "react-native-elements";
 
-const Category = ({ item, key }) => {
+const Category = ({ item, handleCategory }) => {
   return (
     <Badge 
         value={item}
-        onPress={(e) => console.log('pressed',e)} 
-        key={key} 
+        onPress={() =>  handleCategory(item)} 
         containerStyle={{ backgroundColor: "#e1621d" }} 
         wrapperStyle={{ marginRight: 5, marginBottom: 5 }} 
     />
